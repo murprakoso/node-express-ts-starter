1f09e8cc45bd47eabbd3956525c796ea3605d0b2
murprakoso <prakoso.clan04@gmail.com>
2024-03-30 18:19:11 +0700

    implement build with swc compiler

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.json({ message: 'Menyala ✨🎉🚀!!!' })
@@ -11,5 +13,5 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on http://localhost:${PORT}`)
+    console.log(`🚀 Server is running on http://localhost:${PORT} 🎉`)
 })
