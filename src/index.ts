550549e3a3849ef3cb38e12bf16401b837e42074
murprakoso <prakoso.clan04@gmail.com>
2024-03-30 16:47:29 +0700

    done adding express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello, World!')
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Menyala ✨🎉🚀!!!' })
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`)
+})
